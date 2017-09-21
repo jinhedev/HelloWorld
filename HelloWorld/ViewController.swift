9eb7264bf68d3601b808e2f4761ec15b3d6c1bf2
ohjinnyboy <jin@pkfitness.com>
2017-09-20 18:28:58 -0700

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by ohjinnyboy on 9/20/17.
+//  Copyright © 2017 ohjinnyboy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
